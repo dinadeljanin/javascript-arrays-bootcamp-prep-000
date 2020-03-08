0a148a41cb63b06d8a697be1df049a3f5f794b54
D D <ddeljanin5@icloud.com>
2020-03-08 03:36:08 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,39 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  let preservedArr = [...array];
+  return preservedArr.unshift(element);
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, element) {
+  return arr.unshift(element);
+}
+
+function addElementToEndOfArray(array, element) {
+  let preservedArr = [...array]; 
+  return preservedArr.push(element);
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  return array.push(element);
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.shift();
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return array.pop();
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length-1);
+}
